aff5315192428ec09fae32da30010f3e6f9a0bac
Manglam <manglamdubey11@gmail.com>
2026-05-09 12:58:29 +0530

    feat: removed unnecessary Blueprint import

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, redirect, url_for, render_template
+from flask import request, jsonify, redirect, url_for, render_template
 from app import db, bcrypt
 from app.models import User
 from flask_login import login_user, logout_user, login_required
