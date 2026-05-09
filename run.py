50ea742e687e77149a465f1076f668b0cece3e82
Manglam <manglamdubey11@gmail.com>
2026-05-09 15:12:12 +0530

    fix: use socketio.run instead of app.run for WebSocket support

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from flask_migrate import Migrate
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True, host="127.0.0.1", port=5000)
